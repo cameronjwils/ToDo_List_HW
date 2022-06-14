8701104f699bbf5d08e91e88d23cdc9a67ca5a3f
Cameron Wilson <cameronwilson@Camerons-MacBook-Air.local>
2022-06-14 08:35:42 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react';
 
 function App() {
+
+  const [tasks, setTasks] = useState([
+    {name: 'Buy Shopping', priorty: 'high'},
+    {name: 'Hoover House', priorty: 'low'},
+    {name: 'Empty Dishwasher', priorty: 'low'},
+  ]);
+
+  const [newTask, setNewTask] = useState("");
+
+  const taskNodes = tasks.map((task, index) => {
+    return(
+      <li
+        key={index} className={task.priorty ? "high":'low' }>
+        <span>
+          {task.name}
+        </span>
+      </li>
+    )
+  });
+
+  const handleTaskInput = (event) => {
+    setNewTask(event.target.value)
+  };
+
+  const saveNewTask = (event) => {
+    event.preventDefault();
+    const copyTasks = [...tasks]
+    copyTasks.push({name: newTask})
+    setTasks(copyTasks)
+    setNewTask('')
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <h1>ToDo List</h1>
+        <hr></hr>
+
+    <form onSubmit={saveNewTask}>
+            <label htmlFor='new-task'>Add a new Task:</label>
+            <input id='new-task' type='text' value={newTask} onChange={handleTaskInput}/>
+            <input type='submit' value='Save New Task' />
+          </form>
+
+    <ul>
+      {taskNodes}
+    </ul>
+
     </div>
   );
 }
